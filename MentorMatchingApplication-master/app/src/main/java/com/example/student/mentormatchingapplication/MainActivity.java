3a935473bcc6ac6f506e2f9bb1f0494afe63d290
HaesungJeon <eternalasset@ajou.ac.kr>
2018-04-17 00:38:13 +0900

    commit test1

---
+++ MentorMatchingApplication-master/app/src/main/java/com/example/student/mentormatchingapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.student.mentormatchingapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Hello World
+        //Hello World2
+        //Hello World3  test commit test
+    }
+}
